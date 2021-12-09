4c08448fb1327cf37a5249e6ec63f6525af0544a
Luana da Rosa <lrosa.silva@outlook.com>
2021-12-09 16:41:29 -0300

    cria metodos para consultas

--- src/main/java/dao/ProdutoDao.java
+++ src/main/java/dao/ProdutoDao.java
@@ -3,6 +3,7 @@ package dao;
 import model.Produto;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
 import java.util.List;
 
 public class ProdutoDao {
@@ -32,6 +33,29 @@ public class ProdutoDao {
     public List<Produto> buscarTodos(){
         String jpql = "SELECT p FROM Produto p";
         return em.createQuery(jpql, Produto.class).getResultList();
+    }
+
+    public List<Produto> buscarPorNome(String nome){
+        String jpql = "SELECT p FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    //método abaixo não retorna nome do produto
+    public List<Produto> buscarPorNomeDaCategoria(String nome){
+        String jpql = "SELECT p FROM Produto p WHERE p.categoria.nome = :nome";
+        return em.createQuery(jpql, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+
+    //metodo pra carregar 1 atributo apenas de um objeto(entidade)
+    public BigDecimal buscarPrecoDoProdutoPorNome(String nome){
+        String jpql = "SELECT p.preco FROM Produto p WHERE p.nome = :nome";
+        return em.createQuery(jpql, BigDecimal.class)
+                .setParameter("nome", nome)
+                .getSingleResult();
     }
 
 }
--- src/main/java/test/CadastroDeProduto.java
+++ src/main/java/test/CadastroDeProduto.java
@@ -23,8 +23,11 @@ public class CadastroDeProduto {
         Produto p = produtoDao.buscarPorId(1l);
         System.out.println(p.getPreco());
 
-        List<Produto> todos = produtoDao.buscarTodos();
+        List<Produto> todos = produtoDao.buscarPorNomeDaCategoria("CELULARES");
         todos.forEach(p2 -> System.out.println(p.getNome()));
+
+        BigDecimal precoDoProduto = produtoDao.buscarPrecoDoProdutoPorNome("Xiaomi");
+        System.out.println("Preço do produto: " +precoDoProduto);
     }
 
     private static void cadastrarProduto() {
